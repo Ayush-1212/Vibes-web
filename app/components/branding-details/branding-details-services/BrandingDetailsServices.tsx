6cb6527edf51c206f010b779f52c1c224099a0c7
Akash Singh <akash.singh@vibescom.in>
2025-12-31 16:03:46 +0530

    updated branding services

--- app/components/branding-details/branding-details-services/BrandingDetailsServices.tsx
+++ app/components/branding-details/branding-details-services/BrandingDetailsServices.tsx
@@ -10,6 +10,7 @@ type ServiceItem = {
     tags: string[];
     url: string;
     highlighted?: boolean;
+    icon?: string;
 };
 
 const services: ServiceItem[] = [
@@ -61,6 +62,7 @@ const Brandingservices: ServiceItem[] = [
             "Brand Guidelines",
             "Employer Branding",
         ],
+        icon: "/assests/img/branding-details/carporate-icon.png",
         url: "/branding-services/corporate-branding",
         highlighted: true,
     },
@@ -73,6 +75,7 @@ const Brandingservices: ServiceItem[] = [
             "PR, Photoshoot & Videos",
             "Graphic Design",
         ],
+        icon: "/assests/img/branding-details/carporate-icon.png",
         url: "/branding-services/brand-upliftment",
     },
     {
@@ -80,98 +83,69 @@ const Brandingservices: ServiceItem[] = [
         description:
             " Developing  structured brand strategies and communication frameworks that align messaging, tone, and positioning with business goals.",
         tags: [
-            "Logo Design",
-            "Content Creation",
-            "Storytelling",
-            "Brand Positioning",
+            "Rebranding",
+            "PR, Photoshoot & Videos",
+            "Graphic Design",
         ],
+        icon: "/assests/img/branding-details/carporate-icon.png",
         url: "/branding-services/brand-communication-and-strategy",
     },
 ];
 
 const BrandingDetailsServices: React.FC = () => {
     return (
-        <div className="grid grid-cols-3 gap-2 py-4">
-        {
-            [1,2,3].map((_,index) => (
-                <div key={index} className="mb-10">
-                    <BrandingServiceCard service={Brandingservices[index]} />
+        <div className="branding-services-wrapper section-gap">
+            <div className="container mx-auto max-w-screen-xl px-4 md:px-8 ">
+                <div className="grid grid-cols-12 gap-6 items-center">
+
+                    {/* Left Column */}
+                    <div className="col-span-12 lg:col-span-5">
+                        <div className="flex  mb-5">
+                            <div className="px-6 py-2 rounded-full border border-[#F4BE00] inline-flex items-center gap-2">
+                                <span className="w-2 h-2 rounded-full bg-[#F4BE00]" />
+                                <span className="font-poppins font-semibold text-[10px] uppercase text-[#F4BE00]">
+                                    Services Highlight
+                                </span>
+                            </div>
+                        </div>
+
+                        <h3 className=" font-poppins font-medium text-[40px]">End-to-End<br />
+                            <span className=" relative w-fit highlight z-9 font-semibold">Branding Solutions</span>
+                        </h3>
+
+                        <p className="text-[#707070]  max-w-2xl mx-auto mb-16 mt-5">
+                            Vibes delivers strategic branding that turns business vision into powerful, consistent identities, aligning purpose, design, and messaging across every customer touchpoint for lasting recognition growth.
+                        </p>
+                    </div>
+
+                    {/* Right Column */}
+                    <div className="col-span-12 lg:col-span-7">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 place-items-center">
+
+                            {[1, 2, 3].map((_, index) => (
+                                <div
+                                    key={index}
+                                    className={
+                                        index === 0
+                                            ? "sm:col-span-2 flex justify-center"
+                                            : ""
+                                    }
+                                >
+                                    <BrandingServiceCard service={Brandingservices[index]} />
+                                </div>
+                            ))}
+
+                        </div>
+                    </div>
+
                 </div>
-            ))
-        }
-        {/* <BrandingServiceCard service={services[0]} /> */}
+            </div >
         </div>
-    //     <section className=" branding-services-wrapper w-full section-gap">
-    //         <div className="flex justify-center mb-5">
-    //             <div className="
-    //     px-6 py-2 
-    //     rounded-full 
-    //     border border-[#F4BE00] 
-    //     inline-flex items-center gap-2
-    //   ">
-    //                 <span
-    //                     className="w-2 h-2 rounded-full bg-[#F4BE00]"
-    //                 ></span>
-    //                 <span className="font-[Poppins] font-semibold text-[10px] leading-[1.23] uppercase text-[#F4BE00]">
-    //                     Services Highlight
-    //                 </span>
-    //             </div>
-    //         </div>
-    //         <h3 className=" text-center font-poppins font-medium text-[40px] leading-[1.23] tracking-norma]">
-    //             End-to-End <span className="highlight relative z-9 w-fit font-poppins font-semibold text-[40px] leading-[1.23] tracking-normal">Branding Solutions</span>
-
-    //         </h3>
-    //         <p className="text-[#707070] font-normal text-[16px] leading-[130%] tracking-[0] text-center max-w-2xl mx-auto mb-16 mt-5">
-    //             Offering strategic branding solutions that transform business vision into clear, consistent brand identities across every customer touchpoint.
-    //         </p>
-
-
-    //         {
-    //             services.map((item, index) => (
-    //                 <div
-    //                     key={index}
-    //                     className="group transition-colors duration-300 hover:bg-[#E9F1EC]"
-    //                 >
-    //                     <div className="container mx-auto px-4 py-14">
-    //                         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-center">
-    //                             <div className="lg:col-span-4">
-    //                                 <h2 className="font-poppins font-semibold text-[32px] leading-[1.27] tracking-normal text-[#282828] mb-4">
-    //                                     {item.title}
-    //                                 </h2>
-
-    //                                 <div className="flex flex-wrap gap-2 mb-5">
-    //                                     {item.tags.map((tag, tagIndex) => (
-    //                                         <span
-    //                                             key={tagIndex}
-    //                                             className="px-4 py-1 rounded-full bg-[#466E59] text-[#FFFFFF] font-poppins font-medium text-[12px] leading-[1.25] tracking-normal"
-    //                                         >
-    //                                             {tag}
-    //                                         </span>
-    //                                     ))}
-    //                                 </div>
-
-    //                             </div>
-    //                             <div className="lg:col-span-5">
-
-    //                                 <p className="text-[#737373] font-poppins font-medium text-[12px] leading-[1.23] tracking-normal max-w-[520px]">
-    //                                     {item.description}
-    //                                 </p>
-    //                             </div>
-
-    //                             <div className="lg:col-span-3 flex lg:justify-end">
-    //                                 <Button2 className=" cursor-pointer transition-all duration-300 group-hover:translate-x-5 not-odd:mt-5 p-[20px]  hover:border-[#2B4C69] hover:bord[2px] hover:text-[#F4BE00] hover:bg-transparent text-[14px]">
-    //                                     <Link href={item.url}><span>View More</span></Link>
-    //                                     <ArrowUpIcon className="transform rotate-45" />
-    //                                 </Button2>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             ))
-    //         }
-    //     </section >
     );
 };
+
+
+
 
 export default BrandingDetailsServices;
 
@@ -180,11 +154,11 @@ export default BrandingDetailsServices;
 
 const BrandingServiceCard = ({ service }: any) => {
     return (
-        <div className="group relative w-full  h-[278px] bg-[#466E59]/100 shadow-lg transition-all duration-500 rounded-lg hover:bg-[#FFFFFF]">
+        <div className=" border-b-6 border-transparent hover:border-[#204667] transition-colors duration-300 group relative w-[340px]  h-[278px] bg-[#466E59]/100 shadow-lg transition-all duration-500 rounded-lg hover:bg-[#FFFFFF]">
 
             {/* ICON */}
             <div
-                className="group absolute -top-4 right-[-8px] w-18 h-18 z-10 rounded-full flex items-center justify-center transition bg-[#FFFFFF] group-hover:bg-[rgb(70,110,89)]
+                className="group absolute -top-4 right-[-8px] w-18 h-18 z-10 rounded-full flex items-center justify-center transition bg-[#FFFFFF] group-hover:bg-[#F4BE00]
 border-[7px] border-[#ffffff]">
                 <Image
                     src={service.icon}
